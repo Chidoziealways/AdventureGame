b6fa815a1008a15d2c0faf9a435fd4221ca8a9d1
Chidoziealways <uzowuluchidozie@gmail.com>
2024-10-20 19:00:22 +0100

    111100000000000

---
+++ src/main/java/model/ModelLoader.java
@@ -0,0 +1,38 @@
+package model;
+
+import jassimp.*;
+
+import java.io.IOException;
+import java.util.EnumSet;
+
+public class ModelLoader {
+    public static void loadModel(String filePath) throws IOException {
+        AiScene scene = Jassimp.importFile(filePath,
+                EnumSet.of(AiPostProcessSteps.Triangulate,
+                        AiPostProcessSteps.FlipUVs));
+        if (scene == null) {
+            throw new RuntimeException("Error loading model");
+        }
+        processNode((AiNode) scene.getSceneRoot(), scene);
+    }
+
+    private static void processNode(AiNode node, AiScene scene) {
+        int[] meshIndices = node.getMeshes();
+        for (int meshIndex : meshIndices) {
+            AiMesh mesh = scene.getMeshes().get(meshIndex);
+            processMesh(mesh);
+        }
+        for (AiNode childNode : node.getChildren()) {
+            processNode(childNode, scene);
+        }
+    }
+
+    private static void processMesh(AiMesh mesh) {
+        for (int i = 0; i < mesh.getNumVertices(); i++) {
+            float x = mesh.getPositionX(i);
+            float y = mesh.getPositionY(i);
+            float z = mesh.getPositionZ(i);
+            System.out.println("Vertex: [" + x + ", " + y + ", " + z + "]");
+        }
+    }
+}
--- src/main/java/net/chidozie/adventuregame/AdventureGame.java
+++ src/main/java/net/chidozie/adventuregame/AdventureGame.java
@@ -1,51 +1,47 @@
 package net.chidozie.adventuregame;
 
+import model.ModelLoader;
 import net.chidozie.adventuregame.jpanel.ImagePanel;
 import updater.UpdateClient;
-
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.Animator;
 
-public class AdventureGame extends JFrame implements ActionListener, Serializable {
+public class AdventureGame extends JFrame implements ActionListener, Serializable, GLEventListener {
     private Player player;
     private JButton button;
     public static JTextField textField;
-    public AdventureGame(Player player){
-        // Set the title of the JFrame
+    private GLCanvas canvas;
+
+    public AdventureGame(Player player) {
         setTitle("Adventure Game Prologue");
-
-        // Set the size of the JFrame
         setSize(400, 300);
-
-        // Set the default close operation
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Set the Background
         ImageIcon icon = new ImageIcon("");
-
-
-        // Create a JPanel to hold the components
         ImagePanel panel = new ImagePanel(icon.getImage());
 
-        // Create a JButton
         button = new JButton("Start");
-        JLabel label = new JLabel("HEllo");
+        JLabel label = new JLabel("Hello");
         label.setSize(10, 10);
+
         button.setBounds(180, 80, 1000, 100);
-        button.addActionListener(this); // Add ActionListener to the button
+        button.addActionListener(this);
         button.setBackground(Color.BLACK);
-        textField = new JTextField(20); //
-        textField.setBounds(180, 500, 80, 90);
-        JButton button1 = new JButton("SUBMIT");
-        button1.setBounds(180, 80, 100, 100);
         button.setVisible(false);
 
+        textField = new JTextField(20);
+        textField.setBounds(180, 500, 80, 90);
 
-        // Add the button to the panel
+        JButton button1 = new JButton("SUBMIT");
+        button1.setBounds(180, 80, 100, 100);
+
         panel.add(button);
         panel.add(label);
         panel.add(textField);
@@ -55,53 +51,90 @@ public class AdventureGame extends JFrame implements ActionListener, Serializabl
             String jarPath = textField.getText();
             new Thread(() -> {
                 try {
-                   UpdateClient.updateJar();
+                    UpdateClient.updateJar();
                 } catch (Exception ex) {
                     ex.printStackTrace();
-                                  }
+                }
             }).start();
         });
 
-        // Add the panel to the JFrame
         add(panel);
 
+        canvas = new GLCanvas(new GLCapabilities(GLProfile.get(GLProfile.GL2)));
+        canvas.addGLEventListener(this);
+        getContentPane().add(canvas);
 
+        Animator animator = new Animator(canvas);
+        animator.start();
 
-
-        // Make the JFrame visible
         setVisible(true);
         this.player = player;
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Handle button click event
         if (e.getSource() == button) {
-                Start();
-
-              }
+            Start();
+        }
     }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        ModelLoader.loadModel("src/main/resources/your_model.obj");
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        // Render your 3D model here
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {}
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL2 gl = drawable.getGL().getGL2();
+        if (height <= 0) height = 1;
+        float aspect = (float) width / height;
+
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glFrustum(-aspect, aspect, -1.0, 1.0, 1.0, 10.0);
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
     public static void main(String[] args) throws IOException {
         AdventureGame adventureGame = new AdventureGame(new Player());
         UpdateClient.updateJar();
     }
-    public void Start(){
+
+    public void Start() {
         System.out.println("hello");
         button.setVisible(false);
         button.setText("Hello");
         button.setVisible(true);
     }
+
     public void Save(AdventureGame adventureGame) throws FileNotFoundException {
-       try{
-           FileOutputStream fileOutputStream = new FileOutputStream("BinarySaves/Game.svr");
-           ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-           objectOutputStream.writeObject(adventureGame);
-       }catch(IOException e){
-           System.out.println("THere was an Exception" + e);
-       }
+        try (FileOutputStream fileOutputStream = new FileOutputStream("BinarySaves/Game.svr");
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(adventureGame);
+        } catch (IOException e) {
+            System.out.println("There was an Exception: " + e);
+        }
     }
-    public void Load(){
 
-    }public void MainGame(){
+    public void Load() {
+        // Implementation for loading the game
+    }
 
+    public void MainGame() {
+        // Implementation for the main game logic
     }
 }
